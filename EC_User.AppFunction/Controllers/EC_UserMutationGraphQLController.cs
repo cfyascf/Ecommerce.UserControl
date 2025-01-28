d01218d44cfac60126538f1305fa1445898c1752
yasmi <yas1cf1@gmail.com>
2025-01-28 13:17:36 -0300

    feat: user service done

---
+++ EC_User.AppFunction/Controllers/EC_UserMutationGraphQLController.cs
@@ -0,0 +1,30 @@
+using EC_User.AppFunction.Models;
+using EC_User.AppFunction.Services;
+using EC_User.Domain.Entities;
+
+namespace EC_User.AppFunction.Controllers
+{
+    public class EC_UserMutationGraphQLController
+    {
+        private readonly UserService _userService;
+
+        public EC_UserMutationGraphQLController(UserService userService)
+        {
+            _userService = userService;
+        }
+        public async Task<User> CreateUser(UserRegisterPayload payload)
+        {
+            return await _userService.CreateUser(payload);
+        }
+
+        public async Task<User> UpdateUser(User payload)
+        {
+            return await _userService.UpdateUser(payload);
+        }
+
+        public async Task<User> DeleteUser(long id)
+        {
+            return await _userService.DeleteUser(id);
+        }
+    }
+}
---
+++ EC_User.AppFunction/Services/UserService.cs
@@ -0,0 +1,58 @@
+using AutoMapper;
+using EC_User.AppFunction.Models;
+using EC_User.Domain.Entities;
+using EC_User.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EC_User.AppFunction.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<User> CreateUser(UserRegisterPayload payload)
+        {
+            var userMapping = _mapper.Map<User>(payload);
+            var newUser = await _userRepository.CreateUser(userMapping);
+            
+            return newUser;
+        }
+        
+
+        public async Task<User> UpdateUser(User payload)
+        {
+            var updatedUser = await _userRepository.UpdateUser(payload);
+            return updatedUser;
+        }
+
+        public async Task<User> DeleteUser(long id)
+        {
+            var deletedUser = await _userRepository.DeleteUser(id);
+            return deletedUser;
+        }
+
+        public async Task<List<User>> GetUsers(string search)
+        {
+            List<User> users;
+            if(!string.IsNullOrEmpty(search))
+            {
+                users = await _userRepository.GetUsers()
+                            .Where(u => u.Fullname.Contains(search))
+                            .ToListAsync();
+
+                return users;
+            }
+
+            users = await _userRepository.GetUsers().ToListAsync();
+
+            return users;
+        }
+    }
+}
